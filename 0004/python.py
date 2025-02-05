433e9ede19152f6a9cdf7276618a5cd099372b3c
alistechworld <alistechworld40@gmail.com>
2025-02-06 01:31:01 +0500

    sum

---
+++ 0004/python.py
@@ -0,0 +1,5 @@
+# Write a python program that take two inputes and find the sum 
+
+x = int(input("Enter your first number:"))
+y = int(input("Enter the second number:"))
+print(f"The sum of {x} and {y} is:", x + y)
