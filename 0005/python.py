44f10e77772bf662b12f701426172cb04ad64f9a
alistechworld <alistechworld40@gmail.com>
2025-02-06 02:18:02 +0500

    work

---
+++ 0005/python.py
@@ -0,0 +1,6 @@
+# Find the Square of a Number
+
+n = int(input("Enter a number:"))
+
+square = n ** 2
+print(f"The square of {n} is:", square)
---
+++ 0006/python.py
@@ -0,0 +1,9 @@
+# Python program to swap two number
+
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+
+print(f"Befor swapping x = {x} and y = {y}")
+x , y = y, x
+
+print(f"After swapping x = {x} and y = {y}")
